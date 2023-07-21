074e619404aea0f20ab227de50c9fc21118c60ab
David <defaultik.personal@gmail.com>
2023-07-21 17:22:59 +0300

    Made 'More' button for actions for the files; added assets for future work

--- index.php
+++ index.php
@@ -24,30 +24,45 @@
 
             echo "<div id='container'>";
             echo "<button id='back_button' type='button' onclick='history.go(-1)'>Back</button>";
-                foreach($files as $file) {
-                    echo "<div class='box'>";
 
-                    if (is_dir($file)) {
-                        echo "<img class='folder_icon' src='http://localhost/file_explorer/img/rsz_folder.png'>"; 
-                        echo "<a class='folder_link' href='?dir=" . $file . "'>" . $file . "</a>";
+            foreach ($files as $file) {
+                echo "<div class='box'>";
+
+                if (is_dir($file)) {
+                    echo "<img class='folder_icon' src='http://localhost/file_explorer/img/rsz_folder.png'>"; 
+                    echo "<a class='folder_link' href='?dir=" . $file . "'>" . $file . "</a>";
+                } else {
+                    echo "<img class='file_icon' src='http://localhost/file_explorer/img/rsz_file.png'>";
+
+                    if (empty($_GET["dir"])) {
+                        echo "<a class='file_link' href=" . $file . ">" . $file . "</a>";
                     } else {
-                        echo "<img class='file_icon' src='http://localhost/file_explorer/img/rsz_file.png'>";
-
-                        if (empty($_GET["dir"])) {
-                            echo "<a class='file_link' href=" . $file . ">" . $file . "</a>";
-                        } else {
-                            echo "<a class='file_link' href=" . $_GET["dir"] . "/" . $file . ">" . $file . "</a>";
-                        }
-
-                        echo "<p class='file_size'>" . formatSizeUnits(filesize($dir . "/" . $file)) . "</p>";
-                        echo "<button class='edit_button'><img src=img/rsz_more.png></button>";
+                        echo "<a class='file_link' href=" . $_GET["dir"] . "/" . $file . ">" . $file . "</a>";
                     }
 
-                    echo "</div>";
+                    echo "<p class='file_size'>" . size_units(filesize($dir . "/" . $file)) . "</p>";
                 }
+                
+                $file_func = "'" . $file . "'";
+                $file_func = htmlspecialchars($file_func);
+                echo "<button onclick='openDropdown($file_func)' class='file_manage_button' type='button'>";
+                echo "<img src=img/rsz_more.png>";
+                echo "</button>";
+
+                echo "<div data-type='delete_" . $file . "' id='dropdown_links'>";
+                echo "<img src=img/rsz_bin.png>";
+                echo "</div>";
+
+                echo "</div>";
+            }
+
             echo "</div>";
+
+            function delete_file($file) {
+                echo $file;
+            }
             
-            function formatSizeUnits($bytes) {
+            function size_units($bytes) {
                 if ($bytes >= 1073741824) {
                     $bytes = number_format($bytes / 1073741824, 2) . " GB";
                 } elseif ($bytes >= 1048576) {
@@ -65,5 +80,19 @@
                 return $bytes;
             }
         ?>
+
+       <script>
+            function openDropdown(file) {
+                var file_name = "delete_" + file
+                var specific_file = document.querySelector("[data-type='" + file_name + "']");
+
+                // if (specific_file.style.display === "none") { // buggy, works only on second click
+                if (window.getComputedStyle(specific_file, null).getPropertyValue("display") === 'none') {
+                    specific_file.style.display = "flex";
+                } else {
+                    specific_file.style.display = "none";
+                }
+            }
+        </script>
     </body>
 </html>
